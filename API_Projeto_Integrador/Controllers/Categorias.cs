1d2c52565cc260757bc45d061829a20c877b639c
Gabriel.Ferraz <gabriel.ferraz@questores.com.br>
2022-09-08 09:23:59 -0300

    Criado Categoria

---
+++ API_Projeto_Integrador/Controllers/Categorias.cs
@@ -0,0 +1,24 @@
+﻿using API_Projeto_Integrador.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Projeto_Integrador.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Categorias : ControllerBase
+    {
+        private ICategoriaRepository _repository;
+        public Categorias()
+        {
+            _repository = new CategoriaRepository();
+        }
+
+        [HttpGet]   
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get());
+        }
+        
+    }
+}
---
+++ API_Projeto_Integrador/Repositories/CategoriaRepository.cs
@@ -0,0 +1,37 @@
+﻿using API_Projeto_Integrador.Controllers;
+
+namespace API_Projeto_Integrador.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private static List<Categoria> _db = new List<Categoria>()
+        {
+            new Categoria() { Id = 1, Nome = "Teste"}
+        };
+
+        public List<Categoria> Get()
+        {
+            return _db;
+        }
+
+        public Categoria Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
