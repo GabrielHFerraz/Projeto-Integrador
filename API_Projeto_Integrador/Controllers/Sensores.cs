20321d5bbf9a7479c8ddb2243f9accad11120ce5
GabrielHFerraz <gabrielsh38s2@gmail.com>
2022-09-20 01:22:40 -0300

    Endpoint Sensores OK

--- API_Projeto_Integrador/Controllers/Sensores.cs
+++ API_Projeto_Integrador/Controllers/Sensores.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API_Projeto_Integrador.Models;
+using API_Projeto_Integrador.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_Projeto_Integrador.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class Sensores : ControllerBase
     {
+        private ISensorRepository _repository;
+
+        public Sensores()
+        {
+            _repository = new SensorRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get());
+        }
+
+        [HttpGet("/sensor/{id}")]
+        public IActionResult Get(int id)
+        {
+            var sensor = _repository.Get(id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+            return Ok(sensor);
+        }
+
+        [HttpPost("/cadastro/sensor")]
+        public IActionResult Insert([FromBody] Sensor sensor)
+        {
+            _repository.Insert(sensor);
+            return Ok(sensor);
+        }
+
+        [HttpDelete("/delete/sensor")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+
     }
 }
--- API_Projeto_Integrador/Repositories/SensorRepository.cs
+++ API_Projeto_Integrador/Repositories/SensorRepository.cs
@@ -1,22 +1,33 @@
 ﻿using API_Projeto_Integrador.Models;
+using Dapper;
+using Npgsql;
+using System.Data;
 
 namespace API_Projeto_Integrador.Repositories
 {
     public class SensorRepository : ISensorRepository
     {
+        private IDbConnection _connection;
+
+        public SensorRepository()
+        {
+            _connection = new NpgsqlConnection("User ID=postgres;Password=root;Host=localhost;Port=5432;Database=API_Projeto;Pooling=true;Connection Lifetime=0;");
+        }
+
         public List<Sensor> Get()
         {
-            throw new NotImplementedException();
+            return _connection.Query<Sensor>("SELECT * FROM SENSOR").ToList();
         }
 
         public Sensor Get(int id)
         {
-            throw new NotImplementedException();
+            return _connection.QueryFirstOrDefault("SELECT * FROM SENSOR WHERE ID = @id", new { id = id });
         }
 
         public void Insert(Sensor sensor)
         {
-            throw new NotImplementedException();
+            string SQL = ("INSERT INTO SENSOR (Nome,Categoria)Values(@Nome,@Categoria)");
+             _connection.Query(SQL, sensor);
         }
 
         public void Update(Sensor sensor)
@@ -26,7 +37,7 @@ namespace API_Projeto_Integrador.Repositories
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            _connection.Query("DELETE FROM SENSOR WHERE ID = @Id", new {id =id});
         }
     }
 }
